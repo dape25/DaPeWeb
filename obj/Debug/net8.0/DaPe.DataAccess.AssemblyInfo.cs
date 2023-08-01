8e8b53bc76b86e3fd8892edc2865160a771a7a2c
dape25 <danielpetrik@eml.cc>
2023-08-01 11:59:01 +0200

    V50.00

--- obj/Debug/net8.0/DaPe.DataAccess.AssemblyInfo.cs
+++ obj/Debug/net8.0/DaPe.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DaPe.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dde32a2166f75c37756f12c5bdfd2102a03e3fe6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DaPe.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DaPe.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
